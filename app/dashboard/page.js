71a89c513e8f6cb940ccc37d5e439f5f5678f714
Ebun Adebesin <adebesinebunoluwa1@gmail.com>
2024-09-09 01:09:54 -0700

    build fixes

--- app/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-
-
-import sales from '@/app/sales'
-import expenses from '@/app/expenses'
-
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'react-chartjs-2';
-import { Bar, Line } from 'react-chartjs-2';
-
